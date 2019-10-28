1dc7e3bd7194dac5812bcd27867941b6066b6bf3
himanshu <himanshu85k@gmail.com>
2019-10-28 22:36:36 +0530

    first design ready

---
+++ src/app/app.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+// import { TesseractWorker } from 'tesseract.js';
+import { createWorker } from 'tesseract.js';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  TITLE = 'Text Sense';
+  text = '';
+  showSpinner = false;
+  worker: any;
+
+  constructor() {
+    this.worker = createWorker();
+  }
+
+  async recognize(event) {
+    try {
+      this.showSpinner = true;
+      let imageFile = event.target.files[0];
+      await this.worker.load();
+      await this.worker.loadLanguage('eng');
+      await this.worker.initialize('eng');
+      const { data: { text } } = await this.worker.recognize(imageFile);
+      this.text = text;
+      this.showSpinner = false;
+    } catch (e) {
+      alert('Something went wrong while trying to recognize text: ' + e);
+    }
+  }
+
+  async downloadPDF() {
+    try {
+      const filename = 'tesseract-ocr-result.pdf';
+      const { data } = await this.worker.getPDF('Tesseract OCR Result');
+      const blob = new Blob([new Uint8Array(data)], { type: 'application/pdf' });
+      if (navigator.msSaveBlob) {
+        // IE 10+
+        navigator.msSaveBlob(blob, filename);
+      } else {
+        const link = document.createElement('a');
+        if (link.download !== undefined) {
+          const url = URL.createObjectURL(blob);
+          link.setAttribute('href', url);
+          link.setAttribute('download', filename);
+          link.style.visibility = 'hidden';
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+        }
+      }
+    } catch(e) {
+      alert('Something went wrong while trying to download PDF: ' + e);
+    }
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatProgressSpinnerModule,
+    MatCardModule,
+    MatButtonModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
